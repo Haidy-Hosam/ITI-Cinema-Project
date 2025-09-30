23ff3a12733d22d745aabf48bd1c70bc6959e55b
ahmed amir <ahmedgharip2004@gmail.com>
2025-09-30 19:32:13 +0300

    Create empty-watchlist.component.ts

---
+++ empty-watchlist.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-watchlist',
+  templateUrl: './empty-watchlist.component.html',
+  styleUrls: ['./empty-watchlist.component.css']
+})
+export class EmptyWatchlistComponent {}
